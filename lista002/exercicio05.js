b228034cdba76e6110f7feb36a4c3a28977afdc1
Kalline Ferreira <kallinesilva100@gmail.com>
2023-10-09 10:41:33 -0300

    atualização

--- lista002/exercicio05.js
+++ lista002/exercicio05.js
@@ -1,35 +1,60 @@
 // Considere uma pilha que armazene caracteres. Escreva uma função que verifique
 // se uma palavra é uma palíndromo.
 
-function frasePolindromo(){
-   let palavra ='ESSE';
 
-   let separar = palavra.split(' ');
-   console.log(separar)
+function palindromo(){
+    let palavra='esse';
+    let pilha=palavra.split('');
 
-   for (let i = 0; i < separar.length; i++) { //pega cada palavra e separa
-    let letras = separar[i].split('');// tem todas as letras separadas, porem em [] 
-    console.log(letras)
-    let novaPalavra = '';// é para receber a palavra invertida
-
-
-    for (let j = letras.length - 1; j >= 0; j--) {
-        novaPalavra += letras[j];
+    let invertida = '';
+    
+    while(pilha.length>0){
+        invertida+=pilha.pop();
     }
 
-    console.log(novaPalavra)
-
-    if(novaPalavra === palavra){
+    if(invertida===palavra){
         console.log("É um Palíndromo");
-    }else{
-        console.log ("Não ´r um Palíndromo")
-    }
-
+   }else{
+        console.log("Não ´r um Palíndromo");
    }
+}
+palindromo()
+
+
+
+
+
+
+
+
+
+
+// function frasePolindromo(){
+//    let palavra ='ESSE';
+//    console.log(palavra)
+//    let separar = palavra.split(' ');
+//    console.log(separar)
+
+//    for (let i = 0; i < separar.length; i++) { //pega cada palavra e separa
+//     let letras = separar[i].split('');// tem todas as letras separadas, porem em [] 
+//     console.log(letras)
+//     let novaPalavra = '';// é para receber a palavra invertida
+
+
+//     for (let j = letras.length - 1; j >= 0; j--) {
+//         novaPalavra += letras[j];
+//     }
+
+//     console.log(novaPalavra)
+
+//     if(novaPalavra === palavra){
+//         console.log("É um Palíndromo");
+//     }else{
+//         console.log ("Não ´r um Palíndromo")
+//     }
+
+//    }
 
  
-}
-
-
-
-frasePolindromo()
+// }
+// frasePolindromo()
