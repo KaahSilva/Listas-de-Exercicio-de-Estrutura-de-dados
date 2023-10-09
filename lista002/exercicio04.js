b623567ec6a6bda882b7fa6d974568fe68851e6e
Kalline Ferreira <kallinesilva100@gmail.com>
2023-10-09 11:08:33 -0300

    atualização

--- lista002/exercicio04.js
+++ lista002/exercicio04.js
@@ -20,8 +20,8 @@ function inverterPalavras() {
         let novaPalavra = '';// é para receber a palavra invertida
         //console.log(caracteres)
 
-        for (let j = caracteres.length - 1; j >= 0; j--) {
-            novaPalavra += caracteres[j];
+        while(caracteres.length >0) {
+            novaPalavra += caracteres.pop();
         
         }
 
@@ -36,6 +36,43 @@ function inverterPalavras() {
     return novaFrase;
 }
 console.log(inverterPalavras());
+
+
+
+//-------------------
+
+
+// function inverterPalavras() {
+//     let frase = 'ESSE EXERCICIO É MUITO FACIL .';
+    
+//     // Convertendo a string em um array de palavras
+//     let palavras = frase.split(' ');
+//     console.log(palavras)
+//     palavras.pop()
+//     console.log(palavras)
+//     // Invertendo a ordem dos caracteres de cada palavra
+//     for (let i = 0; i < palavras.length; i++) { //pega cada palavra e separa
+//         let caracteres = palavras[i].split('');// tem todas as letras separadas, porem em [] diferentes
+        
+//         let novaPalavra = '';// é para receber a palavra invertida
+//         //console.log(caracteres)
+
+//         for (let j = caracteres.length - 1; j >= 0; j--) {
+//             novaPalavra += caracteres[j];
+        
+//         }
+
+//      palavras[i] = novaPalavra; //recebe todas as palavras so que juntas
+    
+//     }
+//     console.log(palavras)
+//     // Juntando as palavras novamente
+//     let novaFrase = palavras.join(' ');
+   
+
+//     return novaFrase;
+// }
+// console.log(inverterPalavras());
 
 
 //obs: não to conseguindo remover o ponto sem dar espaço pq ele fica dentro de um array e se eu coloca pra remover no for (caracteres.pop)ele reemove sempre a ultima letra de cada array :(
